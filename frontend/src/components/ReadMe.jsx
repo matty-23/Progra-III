285ef5768e14d2c9ad267f03a0494d177212baef
Matty-23 <matu.naser@gmail.com>
2026-04-16 19:33:04 -0300

    Se arreglo las rutas para el ReadME

--- frontend/src/components/ReadMe.jsx
+++ frontend/src/components/ReadMe.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import {useEffect, useState } from 'react';
+import { useLocation } from "react-router-dom";
 import './ReadMe.css';
 
 export default function ReadMe() {
