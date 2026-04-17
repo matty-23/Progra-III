366b29b9f62f3c7744101893bdc29b8c8d591d46
Matty-23 <matu.naser@gmail.com>
2026-04-17 11:24:19 -0300

    Se cambio el estilo del readme-input

--- frontend/src/components/ReadMe.jsx
+++ frontend/src/components/ReadMe.jsx
@@ -2,26 +2,23 @@ import {useEffect, useState } from 'react';
 import { useLocation } from "react-router-dom";
 import './ReadMe.css';
 
+
 export default function ReadMe() {
+
   const { pathname } = useLocation();
-
   const [text, setText] = useState("");
-
   const [content, setContent] = useState(() => 
-    localStorage.getItem('readme-content') || ''
-  );
+    localStorage.getItem('readme-content') || '');
 
   const handleChange = (e) => {
     const newText = e.target.value;
     setContent(newText);
-    localStorage.setItem('readme-content', newText);
-  };
-   useEffect(() => {
+    localStorage.setItem('readme-content', newText);};
+   
+  useEffect(() => {
     const saved = localStorage.getItem(`readme-${pathname}`);
-    setText(saved || "");
-  }, [pathname]);
+    setText(saved || "");}, [pathname]);
 
-  // 🔹 Guardar cuando cambia el texto
   useEffect(() => {
     localStorage.setItem(`readme-${pathname}`, text);
   }, [text, pathname]);
@@ -29,13 +26,12 @@ export default function ReadMe() {
   return (
     <div className="readme-container">
       <h2 className='title-readme'>README</h2>
-      
       <p>
       <textarea
         className="readme-input"
+        id="readme-input"
         value={text}
-        onChange={(e) => setText(e.target.value)}
-      />
+        onChange={(e) => setText(e.target.value)}/>
         </p>
     </div>
   );
